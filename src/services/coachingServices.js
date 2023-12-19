a9a9ffa5725af44de26bebe5abf49ab989c10889
aryaksingh <153924273+aryaksingh@users.noreply.github.com>
2023-12-19 12:04:35 +0000

    ......

--- src/services/coachingServices.js
+++ src/services/coachingServices.js
@@ -1,6 +1,6 @@
 import { API_BASE_URL } from "../config";
 
-export const addNewCoachingForms = async ({data, urlEndpoint}) => {
+export const addNewCoachingForms = async ({ data, urlEndpoint }) => {
   try {
     const response = await fetch(API_BASE_URL + urlEndpoint, {
       method: "POST",
